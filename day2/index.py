90927132a18b91bfe45fbbb88cc8886b439f1569
Mihnea Ciocioiu <mihnea@Mihneas-MacBook-Pro.local>
2024-12-05 00:42:19 +0800

    Day 4 commit.

--- day2/index.py
+++ day2/index.py
@@ -1,10 +1,6 @@
 sol = 0
 
-with open('/Users/mihnea/Projects/AoC/day2/input.txt', 'r') as file:
-    for line in file:
-        list = []
-        for num in map(int, line.split()):
-            list.append(num)
+def checkLine(list):
         increasing = True
         decreasing = True
         valid      = True
@@ -19,6 +15,40 @@ with open('/Users/mihnea/Projects/AoC/day2/input.txt', 'r') as file:
                 valid = False
 
         if (valid and (increasing or decreasing)):
+            return True
+
+with open('/Users/mihnea/Projects/AoC/day2/input.txt', 'r') as file:
+    for line in file:
+        list = []
+        for num in map(int, line.split()):
+            list.append(num)
+        
+        if (checkLine(list)):
             sol += 1
+
+print(sol)
+
+# Second part
+
+sol = 0
+
+with open('/Users/mihnea/Projects/AoC/day2/input.txt', 'r') as file:
+    for line in file:
+        list = []
+        for num in map(int, line.split()):
+            list.append(num)
+        
+        if (checkLine(list)):
+            sol += 1
+            continue
+
+        for i in range(len(list)):
+            newList = []
+            newList += list[:i]
+            newList += list[i+1:]
+
+            if (checkLine(newList)):
+                sol += 1
+                break
 
 print(sol)
